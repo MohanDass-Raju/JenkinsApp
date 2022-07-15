de4dc871165ce026a581ba473acb7498e22bdaae
MohanDass-Raju <mailtomohandasss@gmail.com>
2022-07-15 11:52:52 +0530

    Add project files.

---
+++ JenkinsApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace JenkinsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to jenkins");
+        }
+    }
+}
